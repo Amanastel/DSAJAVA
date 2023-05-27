c3d4ca7fa693df6a2ebb2edccbe11dfe37b430e1
Amanastel <87702404+Amanastel@users.noreply.github.com>
2023-05-28 02:24:09 +0530

    k

--- DSA JAVA/DSA_part1/src/BinarySearch/SmallestLetter.java
+++ DSA JAVA/DSA_part1/src/BinarySearch/SmallestLetter.java
@@ -1,51 +1,30 @@
 package BinarySearch;
 
-public class Ceiling {
+public class SmallestLetter {
+
+    //https://leetcode.com/problems/find-smallest-letter-greater-than-target/
     public static void main(String[] args) {
-//        Scanner sc = new Scanner(System.in);
-//        int n = sc.nextInt();
-//        int target = sc.nextInt();
-//        int[] arr = new int[n];
-//        for(int i=0; i<n; i++){
-//            arr[i] = sc.nextInt();
-//        }
-//        System.out.println(binarySearch(arr,target));
 
-
-        int[] arr2 = {-1,1,2,3,4,5,22,34,56,789};
-        int target = 23;
-        System.out.println(ceiling(arr2,target));
+        char[] arr2 = new char[]{'c','f','j'};
+        char target = 'j';
+        System.out.println(smallestLetter(arr2,target));
     }
 
-    // return the index of Smalled no >= target
-    public static int ceiling(int[] arr, int target) {
+    public static char smallestLetter(char[] letters, char target) {
 
-        // but what if the target is greater then the greatest number in the  array
-        if(target > arr[arr.length-1]){
-            return -1;
-        }
 
         int start = 0;
-        int end = arr.length - 1;
+        int end = letters.length - 1;
 
         while(start  <= end){
-//            int mid = (start + end) / 2;
             int mid = start + (end - start) / 2;
-            if(target < arr[mid]){
+            if(target < letters[mid]){
                 end = mid - 1;
-            } else if (target > arr[mid]) {
+            } else {
                 start = mid + 1;
-            }else {
-                return mid;
             }
         }
-        return start;
+        return letters[start % letters.length] ;
     }
 }
 
-
-/*
-    n = 5
-    target = 1
-    arr = 0 0 0 1 1
- */
