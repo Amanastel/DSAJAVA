cc580119bc2d9d92373eef2b67ff51d9dec183e2
Amanastel <87702404+Amanastel@users.noreply.github.com>
2023-11-01 02:59:08 +0530

    IsomorphicStrings solve this question

--- DSA JAVA/DSA_part1/src/HashMap/IsomorphicStrings.java
+++ DSA JAVA/DSA_part1/src/HashMap/IsomorphicStrings.java
@@ -31,4 +31,28 @@ public class IsomorphicStrings {
         }
         return true;
     }
+
+    public static boolean isIsomorphic2(String s, String t) {
+        if (s.length() != t.length()) return false;
+        HashMap<Character, Character> map = new HashMap<>();
+        HashMap<Character, Boolean> map2 = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char ch1 = s.charAt(i);
+            char ch2 = t.charAt(i);
+
+            if (map.containsKey(ch1)) {
+                if (map.get(ch1) != ch2) {
+                    return false;
+                }
+            } else {
+                if (map2.containsKey(ch2)) {
+                    return false;
+                }
+                map.put(ch1, ch2);
+                map2.put(ch2, true);
+            }
+        }
+        return true;
+    }
+
 }
